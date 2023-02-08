5668c1d20eb5dedef2d42fe330c8fc56f207845d
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-08 20:55:51 +0100

    Implement Tailwind instead of pure css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,5 @@
 import { useForm } from "react-hook-form";
 
-import styles from "./form.module.css";
-
 const Form = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
@@ -14,26 +12,33 @@ const Form = () => {
   // console.log(enteredName, enteredFile, enteredEmail)
 
   return (
-    <section className={styles.formSection}>
-      <p>Use the form below to upload your file</p>
+    <section className="p-8 w-screen h-screen bg-[#221f23]">
+      <p className="pb-6 mt-4 font-bold text-center text-[whitesmoke]">Use the form below to upload your file</p>
 
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.control}>
-          <label htmlFor="name">Your name</label>
-          <input type="text" id="name" {...register("nameRequired", { required: true })} />
-          {errors.nameRequired && <span>This field is required</span>}
+      <form className="w-[60%] p-6 pb-8 my-0 mx-[auto] border border-[whitesmoke] rounded-[20px] bg-[#1f2b37]" onSubmit={handleSubmit(onSubmit)}>
+
+        <div className="pb-6">
+          <label htmlFor="name" className="block font-bold ml-6 mt-4 mr-2 mb-2 text-[whitesmoke]">Your name</label>
+          <input type="text" id="name" {...register("nameRequired", { required: true })} className="block w-[95%] mx-2 my-[auto] p-2 border border-[#ccc] rounded-[10px] focus:outline-none focus:border-[#656565]"/>
+          {errors.nameRequired && <span className="pl-24 font-bold text-[lightcoral]">This field is required</span>}
         </div>
-        <div className={styles.control}>
-          <label htmlFor="email">Your email</label>
-          <input type="email" id="email" {...register("emailRequired", { required: true })}/>
-          {errors.emailRequired && <span>This field is required</span>}
+
+        <div className="pb-6">
+          <label htmlFor="email" className="block font-bold ml-6 mt-2 mr-2 mb-2 text-[whitesmoke]">Your email</label>
+          <input type="email" id="email" {...register("emailRequired", { required: true })} className="block w-[95%] mx-2 my-[auto] p-2 border border-[#ccc] rounded-[10px] focus:outline-none focus:border-[#656565]"/>
+          {errors.emailRequired && <span className="pl-24 font-bold text-[lightcoral]">This field is required</span>}
         </div>
-        <div className={styles.control}>
-          <label htmlFor="file">Upload your file</label>
-          <input type="file" id="file" {...register("fileRequired", { required: true })}/>
-          {errors.fileRequired && <span>This field is required</span>}
+
+        <div className="pb-6">
+          <label htmlFor="file" className="block font-bold ml-6 mt-2 mr-2 mb-2 text-[whitesmoke]">Upload your file</label>
+          <input type="file" id="file" {...register("fileRequired", { required: true })} className="block w-[95%] mx-2 my-[auto] p-2 border border-[#ccc] rounded-[10px] focus:outline-none focus:border-[#656565]"/>
+          {errors.fileRequired && <span className="pl-24 font-bold text-[lightcoral]">This field is required</span>}
         </div>
-        <button>Submit</button>
+
+        <div className="flex justify-center">
+          <button className="block w-[50%] ml-5 mr-0 my-[auto] py-2 px-8 border border-[whitesmoke] rounded-[25px] bg-[#656565] text-[whitesmoke] text-xl font-bold hover:bg-[darkolivegreen]">Submit</button>
+        </div>
+      
       </form>
 
     </section>
